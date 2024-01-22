2d596064060676a1a51b9d1dd4ab8a414505c1c6
Mi <miorarabenasolo24@gmail.com>
2024-01-22 21:40:25 +0300

    data and view for  avg_yield_per_culture_per_plot

--- data.sql
+++ data.sql
@@ -62,6 +62,36 @@ INSERT INTO field (id_field, latitude, longitude, id_owner) VALUES
 (2, 34.0522, -118.2437, 2),
 (3, 41.8781, -87.6298, 3);
 
+---------------------------------------------------------------------------------------------------
+
+-- Insertion des données dans la table "culture"
+INSERT INTO culture (id_culture, name, seed_quantity, yield_quantity, unit, seed_price, yield_price, id_ground_type) VALUES
+(1, 'Culture A', 10.0, 200.0, 1, 5.0, 100.0, 1),
+(2, 'Culture B', 8.0, 180.0, 2, 4.0, 90.0, 2),
+(3, 'Culture C', 12.0, 220.0, 1, 6.0, 110.0, 3);
+
+-- Insertion des données dans la table "plot"
+INSERT INTO plot (id_plot, id_field, area, id_ground_type) VALUES
+(1, 1, 10.5, 1),
+(2, 1, 8.2, 2),
+(3, 2, 15.2, 1),
+(4, 3, 12.0, 3);
+
+-- Insertion des données dans la table "simulation"
+INSERT INTO simulation (id_simulation, id_plot, id_culture, date_simulation) VALUES
+(10, 1, 1, '2024-01-01 10:00:00'),
+(11, 2, 2, '2024-02-02 12:30:00'),
+(12, 3, 1, '2024-03-03 15:45:00'),
+(13, 4, 3, '2024-04-04 18:00:00');
+
+-- Insertion des données dans la table "yield"
+INSERT INTO yield (id_yield, id_simulation, date_yield, quantity) VALUES
+(1, 1, '2024-01-15 14:30:00', 150),
+(2, 2, '2024-02-20 16:45:00', 130),
+(3, 3, '2024-03-25 20:00:00', 180),
+(4, 4, '2024-04-30 22:15:00', 160);
+
+
 
 
 
--- view.sql
+++ view.sql
@@ -12,3 +12,16 @@ JOIN plot p ON s.id_plot = p.id_plot
 JOIN simulation_details sd ON s.id_simulation = sd.id_simulation
 JOIN action a ON sd.id_action = a.id_action
 ORDER BY p.id_plot, s.date_simulation DESC;
+
+
+JOIN ground_type g on g.id_ground_type=p.id_ground_type
+JOIN culture c on c.id_ground_type=g.id_ground_type
+
+
+CREATE VIEW avg_yield_per_culture_per_plot_view AS
+SELECT p.id_plot,s.id_culture,c.name AS culture_name, avg(y.quantity)/p.area as avg_yield
+FROM plot p
+JOIN simulation s on s.id_plot=p.id_plot
+JOIN culture c on c.id_culture=s.id_culture
+JOIN yield y on y.id_simulation=s.id_simulation
+group by p.id_plot,s.id_culture,c.name;
