540d5b488083e821631c97aada9bf9be14072309
pik4booh <prncrobins2003@gmail.com>
2024-01-23 17:28:07 +0300

    Add All Origins Allowed

--- src/main/java/com/culture/API/Controllers/OwnerController.java
+++ src/main/java/com/culture/API/Controllers/OwnerController.java
@@ -5,16 +5,19 @@ import com.culture.API.Repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", methods= {RequestMethod.POST, RequestMethod.GET})
 public class OwnerController {
 
     @Autowired
