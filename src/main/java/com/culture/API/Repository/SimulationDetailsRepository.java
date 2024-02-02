cd2d73d3a20957dcb7bc473c96d2a4bbeccfab71
Aro Andrianimanana <aroandrianimanana@gmail.com>
2024-02-02 16:56:02 +0300

    Simulation Repositories

--- src/main/java/com/culture/API/Repository/SimulationDetailsRepository.java
+++ src/main/java/com/culture/API/Repository/SimulationDetailsRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.culture.API.Models.Action;
+import com.culture.API.Models.Simulation;
 import com.culture.API.Models.SimulationDetails;
 
 @Repository
 public interface SimulationDetailsRepository extends JpaRepository<SimulationDetails , Integer>{
     SimulationDetails save(SimulationDetails simulation);
     List<SimulationDetails> findAll();
+    List<SimulationDetails> findAllBySimulation(Simulation simulation);
+    List<SimulationDetails> findAllBySimulationAndRessource_Action(Simulation simulation, Action action);
 }
