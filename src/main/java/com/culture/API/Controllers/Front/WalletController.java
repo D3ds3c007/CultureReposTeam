7a497be9eed118ba1cca1bdba33f3393f0336c22
Raitra Fiorenana <raitra007@gmail.com>
2024-02-10 12:06:13 +0300

    Add CORS to WalletController

--- src/main/java/com/culture/API/Controllers/Front/WalletController.java
+++ src/main/java/com/culture/API/Controllers/Front/WalletController.java
@@ -8,10 +8,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +26,10 @@ import com.culture.API.Repository.WalletTransactionRepository;
 
 import jakarta.transaction.Transactional;
 
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", methods= {RequestMethod.POST, RequestMethod.GET})
 public class WalletController {
 
     @Autowired
